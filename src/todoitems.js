2433972dc9cdcc9a5030c08e457746aaef110c47
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-11-23 16:27:00 +0100

    Add functionality to toggle complete status of Todo Item

--- src/todoitems.js
+++ src/todoitems.js
@@ -1,28 +1,38 @@
 export default class TodoItem {
   
-  constructor(title, description, dueDate, priority){
+  constructor(title, description, dueDate, priority, completeStatus){
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.myLists = [];
+    this.completeStatus = false;
   }
 
   editTitle(newTitle){
     this.title = newTitle;
   }
   editDescription(newDesc){
-    this.title = newDesc;
+    this.description = newDesc;
   }
   editDate(newDate){
-    this.title = newDate;
+    this.dueDate = newDate;
   }
+  
   editPriority(newPrio){
-    this.title = newPrio;
+    this.priority = newPrio;
   }
   
   assignLists(lists){
     this.myLists.push(lists);
+  }
+  
+  toggleStatus() {
+    if(this.completeStatus == false){
+      this.completeStatus = true;
+    }else{
+      this.completeStatus = false
+    }
   }
 
 }
