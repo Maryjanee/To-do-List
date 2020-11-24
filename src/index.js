dd8cacd5276a99dab8f021d8712d82a15d511ec9
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-11-24 10:37:52 +0100

    Add navigation content and add styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import List from './lists';
 import TodoItem from './todoitems';
+import addBtn from './assets/plus.svg';
+
 import './styles.scss';
+
+const navBar = document.querySelector('nav');
+const img = document.createElement('img');
+img.src = addBtn;
+
+navBar.appendChild(img);
 
 const allLists = [];
 const defaultList = new List('Tasks');
