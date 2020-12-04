f9a08afba05c7f214e712777c7c358db67fc9ea4
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-12-04 16:07:48 +0100

    Add more tests for createTasks

--- src/test/createTasks.test.js
+++ src/test/createTasks.test.js
@@ -4,9 +4,11 @@ import List from '../lists'
 describe('Create a task', () => {
   let allLists = []
   const all = new List('All')
-  const defaultCat = new List('Default')
+  const defaultCat = new List('Default');
+  const work = new List ('Work');
   allLists.push(all)
   allLists.push(defaultCat)
+  allLists.push(work)
   let currentCategory = allLists[0]
   
   test('it creates and adds task to all list', () => {
@@ -15,4 +17,24 @@ describe('Create a task', () => {
     expect(allLists[0].todos).toContainEqual(task);
     
   });
+  
+  test('it creates and adds task to default list', () => {
+    createTask('title', 'description', 'priority', 'Default', 'duedate', allLists, currentCategory);
+    const task = new Task('title', 'description', 'priority', 'Default', 'duedate');
+    expect(allLists[1].todos).toContainEqual(task);
+    
+  });
+  
+    test('it creates a task but the task should not be added to work', () => {
+    createTask('title', 'description', 'priority', 'Default', 'duedate', allLists, currentCategory);
+    const task = new Task('title', 'description', 'priority', 'Default', 'duedate');
+    expect(allLists[0].todos).toContainEqual(task);
+    expect(allLists[2].todos).not.toContainEqual(task);
+    
+  });
+  
+  
 })
+
+
+
